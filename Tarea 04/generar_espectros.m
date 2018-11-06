5f086da8cc966473cc4f7d2013d32563bb3c1efd
ppizarror <pablo.pizarro@ing.uchile.cl>
2018-11-06 19:10:24 -0300

    Agrega código espectro

---
+++ Tarea 04/generar_espectros.m
@@ -0,0 +1,15 @@
+% Crea los espectros de la tarea
+
+%% Crea los datos
+r0 = 11;
+suelo = 'a';
+zona = 3;
+categoria = 2;
+tx = 1.136;
+ty = 0.757;
+
+%% Crea el espectro elastico
+espectro_elastico(categoria, zona, suelo, 10, true, 3, true);
+
+%% Crea el espectro inelastico
+% espectro_inelastico(categoria, zona, suelo, tx, ty, r0, 10, true, 5);
